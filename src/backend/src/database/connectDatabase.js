0264c5d833ea359d5cedd2bce957d5c531637f82
Noor Chasib <noorchasib@gmail.com>
2023-12-17 16:18:42 -0800

    Update connectDatabase.js

--- src/backend/src/database/connectDatabase.js
+++ src/backend/src/database/connectDatabase.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST || "localhost",
+  host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME
 });
