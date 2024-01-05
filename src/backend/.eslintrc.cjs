7b4530548bcf27b663f6f1b55e50197ded82d186
Noor Chasib <noorchasib@gmail.com>
2024-01-04 21:42:52 -0800

    update dependencies

--- src/backend/.eslintrc.cjs
+++ src/backend/.eslintrc.cjs
@@ -2,24 +2,21 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  plugins: [
-    "@typescript-eslint",
-    "prettier"
-  ]
+  plugins: ['@typescript-eslint', 'prettier'],
 };
