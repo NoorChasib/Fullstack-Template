9b907e31735469be89cfad44f071a04d2bef12f5
Noor Chasib <noorchasib@gmail.com>
2024-01-26 21:53:40 -0800

    Update connectDatabase.ts

--- src/backend/src/database/connectDatabase.ts
+++ src/backend/src/database/connectDatabase.ts
@@ -5,7 +5,7 @@ const client = new Client({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT as string),
+  port: Number(process.env.DATABASE_PORT),
 });
 
 const delayConnect = (): void => {
